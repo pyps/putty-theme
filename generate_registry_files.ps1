0e40cd9bf2cc04affbcaa298743e0ef45d3e2ca1
pyps <gh@pyps.de>
2015-07-30 15:48:58 +0200

    Added silent parameter

--- generate_registry_files.ps1
+++ generate_registry_files.ps1
@@ -3,5 +3,5 @@ $template = "themes/solarized_dark.reg.template"
 
 foreach($sess in $Sessions){
         (Get-Content $template) -replace "__SessionName__" , $sess | Set-Content "generated/theme_${sess}.reg"
-            regedit.exe "generated/theme_${sess}.reg"
+            regedit.exe -s "generated/theme_${sess}.reg"
 }
